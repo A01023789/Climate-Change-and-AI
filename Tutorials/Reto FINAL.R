3079be7cb509d53445c4408c1de43e72b6f59a7d
A01023789 <A01023789@itesm.mx>
2021-06-08 20:23:40 -0500

    Update Reto FINAL.R
    Reto final sólo incluye modelo lineal y obtenemos 3 modelos con el mismo error, falta revisar

--- Tutorials/Reto FINAL.R
+++ Tutorials/Reto FINAL.R
@@ -8,7 +8,7 @@ summary(data)
 
 #Limpiar columnas
 bad.vars<-sapply(data, function(x) {mean(ifelse(is.na(x)==TRUE,1,0))})
-bad.vars<-names(bad.vars[bad.vars>0.50])
+bad.vars<-names(bad.vars[bad.vars>0.03])
 bad.vars<-c(bad.vars,"SE.ENR.PRSC.FM.ZS","SE.PRM.CMPT.ZS","SE.SEC.ENRR","SH.DYN.AIDS.ZS","TT.PRI.MRCH.XD.WD","TX.VAL.TECH.MF.ZS")
 
 #Respuesta de interés, en este caso es WASTE
@@ -17,25 +17,52 @@ response<-"m_waste"
 predictors<-subset(colnames(data),!(colnames(data)%in%c(ids,response,bad.vars)))
 predictors
 length(predictors)
+summary(data[,response])
 
-#Estimación del modelo
-data.model<-data[,c(response,predictors)]
+threshold<- 0.5
+#Guardar como un factor
+data$response.binary<-as.factor(ifelse(data[,response]>threshold,"High","Low"))
+summary(data$response.binary)
+
+# remove NA values in the response
+#Quitar todos los NA de la variable de interés
+data<-subset(data,is.na(response.binary)==FALSE)
+
+#now look at how many high emission cases we have
+#Resumen de la respuesta binaria
+summary(data$response.binary)
+
+data.model<-data[,c("response.binary",predictors)]
 head(data.model)
-#Modelo como una formula de la mitigación de residuos en funcion de esta información
-model<-as.formula(paste0(response,"~",paste(predictors,collapse="+")))
-model
+#eliminar NA's
+data.model<-data.model[complete.cases(data.model),]
+summary(data.model)
+dim(data.model)
 
-full.model <- lm(model, data = data.model)
-summary(full.model)
+#MODELO Lineal
+#Estimación del modelo
+#estimate full model
+data.modelml<-data[,c(response,predictors)]
+head(data.modelml)
+#Modelo como una formula de las emisiones per capita en funcion de esta información
+#Remover las variables altamente relacionadas
+modelml<-as.formula(paste0(response,"~",paste(predictors[1:20],collapse="+")))
 
-#Al correr la regresión lineal se puede apreciar que las variables más significativas son a_agriculture, a_water, m_lulucf, migration_displacement, AG.SRF.TOTL.K2, DT.DOD.DECT.CD, NE.EXP.GNFS.ZS, NY.GDP.DEFL.KD.ZG, SH.DYN.MORT* y SP.ADO.TFRT
+
+
+#Ya no salen NA's pero tampoco hay variables muy significativas
+full.modelml <- lm(modelml, data = data.modelml)
+summary(full.modelml)
+dim(data.modelml)
+
+#Al correr la regresión lineal se puede apreciar que las variables más significativas son a_agriculture, a_urban, m_agriculture, m_energy
 #Se intentará correr un modelo más pequeño
 
 library(leaps)
 
 #Dividiremos la muestra en un conjunto de entrenamiento y uno de prueba para probar el modelo
 set.seed (55555)
-train <- sample (c(TRUE ,FALSE), nrow(data.model ),rep=TRUE)
+train <- sample (c(TRUE ,FALSE), nrow(data.modelml ),rep=TRUE)
 train
 test  <- (!train )
 test
@@ -45,13 +72,13 @@ max.vars<-5
 
 #Para la construcción del modelo se utilizarán 3 tecnicas para determinar cual es mejor, una selección completa, una a través de la tecnica forward y una a través de backward
 #Selección completa
-regfit.best <- regsubsets (model, data.model[train,], nvmax =max.vars,really.big = T) 
+regfit.best <- regsubsets (modelml, data.modelml[train,], nvmax =max.vars,really.big = T) 
 
 #Técnica forward stepwise selection
-regfit.fwd <- regsubsets (model, data.model[train,], nvmax =max.vars,really.big = T, method = "forward") 
+regfit.fwd <- regsubsets (modelml, data.modelml[train,], nvmax =max.vars,really.big = T, method = "forward") 
 
 #Técnica backard stepwise selection
-regfit.bwd <- regsubsets (model, data.model[train,], nvmax =max.vars,really.big = T, method = "backward")
+regfit.bwd <- regsubsets (modelml, data.modelml[train,], nvmax =max.vars,really.big = T, method = "backward")
 
 #Exploraremos cada uno de ellos
 msize<-5
@@ -59,17 +86,18 @@ coef(regfit.best ,msize)
 coef(regfit.fwd , msize)
 coef(regfit.bwd , msize)
 
-#Observamos que con el método completo selecciona las variables a_agriculture, a_urban, a_water, m_agriculture, m_energy
-#Con el método forward stepwise: a_coastal_zone, m_agriculture, m_energy, NE.GDI.TOTL.ZS y SP.DYN.LEOO.IN
-#Con el método backard stepwise: a_agriculture, a_transport, a_water, m_agriculture, m_energy
+#Observamos que con el método completo selecciona las variables a_energy, a_urban, a_water, m_buildings, m_lulucf
+#Con el método forward stepwise: a_energy, a_urban, m_buildings, m_energy, m_lulucf
+#Con el método backard stepwise: a_energy, a_urban, a_water, m_buildings, m_lulucf
+#Se obtuvo que por dos técnicas tenemos que las mejores variables para la predicción son a_energy, a_urban, a_water, m_buildings, m_lulucf
 
 #Para determinar cuál de lo tres modelos es mejor:
 predict.regsubsets <-function (object, model ,newdata ,id ){
   object<-regfit.best
-  newdata<-data.model[test ,]
+  newdata<-data.modelml[test ,]
   #Quiero el modelo con 4 predictores
   id<-5
-  form<-model
+  form<-modelml
   options(na.action='na.pass')
   #Dame una matriz con esta formula y estos datos
   mat<-model.matrix (form,newdata )
@@ -87,14 +115,14 @@ predict.regsubsets <-function (object, model ,newdata ,id ){
 #Estimaremos la tasa de error de los tres modelos para saber cual tiene el menor error
 cv.best<-data.frame(subset.type="best",
                     nvars=1,
-                    test.mse=predict.regsubsets(regfit.best,model,data.model[test ,],1))
+                    test.mse=predict.regsubsets(regfit.best,modelml,data.modelml[test ,],1))
 cv.best
 
 for(i in 2:max.vars)
 {
   pivot<-data.frame(subset.type="best",
                     nvars=i,
-                    test.mse=predict.regsubsets(regfit.best,model,data.model[test ,],i))
+                    test.mse=predict.regsubsets(regfit.best,modelml,data.modelml[test ,],i))
   cv.best<-rbind(cv.best,pivot)
   
 }
@@ -104,6 +132,28 @@ cv.best
 subset(cv.best,test.mse==min(test.mse))
 #actual model
 coef(regfit.best ,subset(cv.best,test.mse==min(test.mse))$nvars)
+
+#backward method
+cv.bwd<-data.frame(subset.type="bwd",
+                   nvars=1,
+                   test.mse=predict.regsubsets(regfit.bwd,modelml,data.modelml[test ,],1))
+
+for(i in 2:max.vars)
+{
+  pivot<-data.frame(subset.type="bwd",
+                    nvars=i,
+                    test.mse=predict.regsubsets(regfit.bwd,modelml,data.modelml[test ,],i))
+  cv.bwd<-rbind(cv.bwd,pivot)
+  
+}
+
+#best model
+subset(cv.bwd,test.mse==min(test.mse))
+#actual model
+coef(regfit.bwd ,subset(cv.bwd,test.mse==min(test.mse))$nvars)
+
+
+
 
 
 
